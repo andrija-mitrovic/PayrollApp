acf5c853f9c3d4d9d06afc23edfee4ba34773ba9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-26 16:05:06 +0200

    Add admin model

---
+++ src/Payroll.Application/Models/Admin.cs
@@ -0,0 +1,30 @@
+﻿namespace PayrollApp.Application.Models
+{
+    public class Admin : Staff
+    {
+        private const float OVERTIME_RATE = 15.5f;
+        private const float ADMIN_HOURLY_RATE = 30;
+
+        public Admin(string name) : base(name, ADMIN_HOURLY_RATE)
+        {
+        }
+
+        public float Overtime { get; private set; }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+            {
+                Overtime = OVERTIME_RATE * (HoursWorked - 160);
+                TotalPay += Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", {nameof(Overtime)} = {Overtime}";
+        }
+    }
+}
