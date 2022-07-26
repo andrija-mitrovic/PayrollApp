445a2bb6b3ebb89f6c76bc5480bafede315a6620
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-26 16:05:14 +0200

    Add manager model

---
+++ src/Payroll.Application/Models/Manager.cs
@@ -0,0 +1,30 @@
+﻿namespace PayrollApp.Application.Models
+{
+    public class Manager : Staff
+    {
+        private const float MANAGER_HOURLY_RATE = 50;
+
+        public Manager(string name) : base(name, MANAGER_HOURLY_RATE)
+        {
+        }
+
+        public int Allowance { get; private set; }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            Allowance = 1000;
+
+            if (HoursWorked > 160)
+            {
+                TotalPay += 1000;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", {nameof(Allowance)} = {Allowance} ";
+        }
+    }
+}
